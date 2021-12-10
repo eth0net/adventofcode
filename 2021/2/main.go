b395cf82368f06c91c8a9b9bfa8635d9b907ebf4
Elliot Thomas <elliot.thomas@unity3d.com>
2021-12-10 12:02:22 +0000

    solve 2021 puzzle 4

--- 2021/2/main.go
+++ 2021/2/main.go
@@ -18,18 +18,27 @@ var input string
 func main() {
 	lines := strings.Split(input, "\n")
 
-	sub := submarine{}
+	var (
+		sub1 submarine
+		sub2 submarine2
+	)
 
 	for _, line := range lines {
 		if line == "" {
 			continue
 		}
-		if err := sub.command(line); err != nil {
-			log.Fatalln("failed to run command:", err)
+
+		if err := sub1.command(line); err != nil {
+			log.Fatalln("sub1: failed to run command:", err)
+		}
+
+		if err := sub2.command(line); err != nil {
+			log.Fatalln("sub2: failed to run command:", err)
 		}
 	}
 
-	log.Printf("Sub position: %d\n", sub.horizontal*sub.vertical)
+	log.Printf("Sub1 position: %d\n", sub1.horizontal*sub1.vertical)
+	log.Printf("Sub2 position: %d\n", sub2.horizontal*sub2.vertical)
 }
 
 type submarine struct {
@@ -58,6 +67,34 @@ func (s *submarine) command(commands ...string) error {
 	return nil
 }
 
+type submarine2 struct {
+	horizontal, vertical, aim int
+}
+
+func (s *submarine2) command(commands ...string) error {
+	for _, cmd := range commands {
+		direction, distance, err := parseCommand(cmd)
+		if err != nil {
+			return fmt.Errorf("error parsing command: %w", err)
+		}
+
+		switch direction {
+		case directionUp:
+			s.aim -= distance
+		case directionDown:
+			s.aim += distance
+		case directionBack:
+			s.horizontal -= distance
+			s.vertical -= distance * s.aim
+		case directionForward:
+			s.horizontal += distance
+			s.vertical += distance * s.aim
+		}
+	}
+
+	return nil
+}
+
 func parseCommand(cmd string) (direction string, distance int, err error) {
 	f := strings.Fields(cmd)
 
@@ -68,7 +105,7 @@ func parseCommand(cmd string) (direction string, distance int, err error) {
 		return "", 0, err
 	}
 
-	return direction, distance, err
+	return direction, distance, nil
 }
 
 const (
